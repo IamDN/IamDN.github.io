919b88bb90ab96671b7af0cb4a83485fab589531
Neduecker <davidneudecker@gmail.com>
2022-11-16 12:33:24 +0100

    WIP2

--- js/app.js
+++ js/app.js
@@ -1,10 +1,30 @@
+
+fetch('./data.json')
+    .then((response) => response.json())
+    .then((json) => load( json));
+
 /* draggable element */
+let dataset = [];
+function load(data) {
+    console.log(data);
+    let box = document.getElementById("actions");;
+    let x = box.offsetLeft + 200;
+    let y =box.offsetTop + 200;
 
-
-for (var i=0; i<5;i++)
-{
-  var item = document.getElementById("action" + (i+1));;
-  item.addEventListener('dragstart', dragStart);
+    for (var i=0; i<data.action.length;i++)
+    {
+        let div = document.createElement("div");
+        div.id = data.action[i].id;
+        div.className = "item action" + i;
+        div.draggable = "true";
+        div.style.left = x + Math.round((150*Math.cos(i*(2*Math.PI/6))))-60 + 'px';
+        div.style.top = y+ Math.round((150*Math.sin(i*(2*Math.PI/6))))-60 + 'px';
+        div.style.setProperty(  "background-color", data.action[i].color);
+        div.addEventListener('dragstart', dragStart);
+        div.innerHTML = "<br>" +data.action[i].name;
+        box.appendChild(div); 
+    }
+    dataset = data;
 }
 
 function dragStart(e) {
@@ -35,7 +55,7 @@ function dragEnter(e) {
 
 function dragOver(e) {
     e.preventDefault();
-    if  (e.target.id === "actions" || e.arget.id === "arena"){
+    if  (e.target.id === "actions" || e.target.id === "arena"){
        e.target.classList.add('drag-over');
     }
 }
@@ -53,36 +73,70 @@ function drop(e) {
 
     // get the draggable element
     const id = e.dataTransfer.getData('text/plain');
-    
     const draggable = document.getElementById(id);
+    
+    let obj = dataset.action.find(item => item.id === id);
+    console.log(obj.outcome);
+    for(i=0; i <  obj.outcome.length; i++)
+    {
+        createDiv(obj.outcome[i], i, obj);
+    }
     // add it to the drop target
     if(e.target.id == "arena" || e.target.id == "actions")
     {
-    e.target.insertBefore(draggable, e.target.firstChild);
+       e.target.insertBefore(draggable, e.target.firstChild);
+      
+       // display the draggable element
+       updateNodes(e.target,draggable);
+   
+    }  
+    draggable.classList.remove('hide');
+}
+function createDiv(outcome, i, obj) {
 
-    // display the draggable element
-    updateNodes(e.target,draggable);
-    }  draggable.classList.remove('hide');
+    var arena =  document.getElementById("arena");
+    var arenaNodes =arena.childNodes;
+    let arenax = arena.offsetLeft + 200;
+    let arenay =arena.offsetTop + 200;
+
+    let div = document.createElement("div");
+    div.id = outcome + i;
+    div.className = outcome + i;
+    //div.draggable = "true";
+    let count= arenaNodes.length;
+    let r = 12* (count+ 3);
+    div.style.left = arenax + Math.round((r*Math.cos(arenaNodes.length*(2*Math.PI/9))))-25 + 'px';
+    div.style.top = arenay+ Math.round((r*Math.sin(arenaNodes.length*(2*Math.PI/9))))-25 + 'px';
+    div.style.setProperty(  "background-color", obj.color);
+    div.style.setProperty('height',80 + 'px');
+    div.style.setProperty('width', 80 + 'px');
+    div.style.setProperty('border-radius', 40 + '%');
+    div.style.setProperty('position', 'absolute');
+    div.style.setProperty('color', 'white');
+    div.style.setProperty('text-align', ' center');
+    div.style.setProperty('font-size', '10px');
+    div.style.setProperty('opacity', '50%');
+    div.innerHTML = "<br>" +outcome;
+    //div.addEventListener('dragstart', dragStart);
+    var arena =  document.getElementById("arena");
+    arena.appendChild(div); 
 }
 
 function updateNodes(target,draggable)
-{
-  var arenaNodes = document.getElementById("arena").childNodes;
-  console.log(arenaNodes);
-  for(i=0; i <  arenaNodes.length; i++)
+{ 
+   // Update actions
+   var actions = document.getElementById("actions");
+   var actionsNodes =actions.childNodes;
+   let actionsx = actions.offsetLeft + 200;
+   let actionsy =actions.offsetTop + 200;
+for(i=0; i <  actionsNodes.length; i++)
  {
-    var size = 100 + (arenaNodes.length -i) * 40;
-    arenaNodes[i].style.setProperty('height',size + 'px');
-    arenaNodes[i].style.setProperty('width', size + 'px');
-    arenaNodes[i].style.setProperty('position', 'absolute');
-    arenaNodes[i].style.setProperty('opacity', '100%');
- }
-
-  if  (target.id === "actions"){
-    draggable.style.setProperty('height','100px');
-    draggable.style.setProperty('width', '100px');
-    draggable.style.setProperty('position', 'relative');
-    draggable.style.setProperty('opacity', '100%');
+    actionsNodes[i].style.setProperty('height', 120 + 'px');
+    actionsNodes[i].style.setProperty('width', 120 + 'px');
+    actionsNodes[i].style.left = actionsx + Math.round((150*Math.cos(i*(2*Math.PI/6))))-60 + 'px';
+    actionsNodes[i].style.top = actionsy+ Math.round((150*Math.sin(i*(2*Math.PI/6)))) -60+ 'px';
+    actionsNodes[i].style.setProperty('opacity', '100%');
+    actionsNodes[i].classList.remove('hide');
   }
 
 }
