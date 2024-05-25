d04ef821720ae94fc7415f219680f9334e19e747
Neduecker <davidneudecker@gmail.com>
2024-05-25 20:35:24 +0200

    Update app.js

--- da/js/app.js
+++ da/js/app.js
@@ -2,7 +2,7 @@ var lock = true;
 var isMobile = false;
 var originalSize = 0;
 var isLeftHalf = true;
-var version = "0.2.2";
+var version = "0.2.3";
 var mouseOverVerb = false;
 var mouseOverNoun = false;
 
